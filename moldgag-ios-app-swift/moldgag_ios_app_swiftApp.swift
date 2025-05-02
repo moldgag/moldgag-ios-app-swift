83f9c52a2ff63ebbb3dd89a96c5fb2410e59cfe4
at <adriantabirta@gmail.com>
2025-05-02 15:35:18 +0300

    xcode project init

---
+++ moldgag-ios-app-swift/moldgag_ios_app_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  moldgag_ios_app_swiftApp.swift
+//  moldgag-ios-app-swift
+//
+//  Created by Adrian Tabirta on 02.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct moldgag_ios_app_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
